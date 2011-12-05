fde7b6d8486551ea3b67aac79fc045455c193e11
Olle Dahlström <olle.dahlstrom@gmail.com>
2011-12-05 13:05:09 +0100

    JSLint refactoring --> Enter bug

--- pp-scripts/projectplace-interface-eventhandler.js
+++ pp-scripts/projectplace-interface-eventhandler.js
@@ -1,4 +1,5 @@
-
+/*jslint browser: true, indent:4 */
+/*global views*/
 var mainViewConfig = {
 	TRENDINGBYCOMMENTS: 'trendingByComments',
 	TRENDINGBYLIKES: 'trendingByLikes',
@@ -7,39 +8,39 @@ var mainViewConfig = {
 	ALLPEOPLESVIEW: 'allpeoplesview',
 	SINGLEPERSONVIEW: 'singlepersonview',
 	ABOUTME: 'aboutme'
-}
+};
 
 var mainViewNavigation = {
 	_view: null,
 	_goto: null,
-	go: function(e){
+	go: function (e) {
 		mainViewNavigation._goto = e.srcElement.getAttribute('goto');
 		mainViewNavigation._view = e.srcElement.getAttribute('view');
 		mainViewNavigation._handleView();
 	},
-	_handleView: function(){
+	_handleView: function () {
 		
-		if(mainViewNavigation._view == mainViewConfig.SINGLECONVERSATION){
+		if (mainViewNavigation._view == mainViewConfig.SINGLECONVERSATION) {
 			views.singleConversationsView(mainViewNavigation._goto);
 			return false;
 		}
-		if(mainViewNavigation._view == mainViewConfig.CONVERSATIONS){
+		if (mainViewNavigation._view == mainViewConfig.CONVERSATIONS) {
 			views.allConversationsView();
 			return false;
 		}
-		if(mainViewNavigation._view == mainViewConfig.ALLPEOPLESVIEW){
+		if (mainViewNavigation._view == mainViewConfig.ALLPEOPLESVIEW) {
 			views.allPeoplesView();
 			return false;
 		}
-		if(mainViewNavigation._view == mainViewConfig.ABOUTME){
+		if (mainViewNavigation._view == mainViewConfig.ABOUTME) {
 			views.aboutView();
 			return false;
 		}
-		if(mainViewNavigation._view == mainViewConfig.SINGLEPERSONVIEW){
+		if (mainViewNavigation._view == mainViewConfig.SINGLEPERSONVIEW) {
 			views.singlePersonView(mainViewNavigation._goto);
 			return false;
 		}
 	}
-}
+};
 
-var singleConversation = {}
+var singleConversation = {};
