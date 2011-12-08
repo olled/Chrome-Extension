4315f6376f1c1b17f260c6cc13b2e5c38c291b05
Olle Dahlström <olle.dahlstrom@gmail.com>
2011-12-08 21:30:06 +0100

    BETA TAG

--- pp-scripts/projectplace-view-controller.js
+++ pp-scripts/projectplace-view-controller.js
@@ -1,60 +1,61 @@
- /**
- * Holds the views of the extension
- * Heavy use of the class ProjectplaceAPICall found in projectplace-api.calls.js
- */
+/*jslint browser: true, indent:4 */
+/*global ProjectplaceAPICall, UserInfo */
+/**
+* Holds the views of the extension
+* Heavy use of the class ProjectplaceAPICall found in projectplace-api.calls.js
+*/
 var PPAPI = new ProjectplaceAPICall();
 
- /**
-  * ProjectplaceViewController - gets the data needed for the different views
-  * 	Currently
-  * 	* loadInitialView == Loads the initial data needed, project info, conversations from all projects, user info.
-  * 	
-  */
- var ProjectplaceViewController = {
- 	totalNewPosts: 0,
-	user:null,
-	
+/**
+* ProjectplaceViewController - gets the data needed for the different views
+*	Currently
+*	* loadInitialView == Loads the initial data needed, project info, conversations from all projects, user info.
+*
+*/
+var ProjectplaceViewController = {
+	totalNewPosts: 0,
+	user: null,
 	/**
 	 * Loads all the data that is needed in the Main Conversations view
 	 * @param {Object} callBack callback function, if any
 	 */
-	loadDataInitialView: function(callBack){
+	loadDataInitialView: function (callBack) {
+	
+		/**
+		* Get the logged in users profile.
+		* 
+		* @param {Object} text - JSONTEXT
+		* @param {Object} xhr - XHR Object
+		*/
+		PPAPI.getMyProfile(function (text, xhr) {
+			ProjectplaceViewController.user = new UserInfo(text);
 		
 		/**
-		 * Get the logged in users profile.
-		 * 
-		 * @param {Object} text - JSONTEXT
-		 * @param {Object} xhr - XHR Object
-		 */
-		PPAPI.getMyProfile(function(text, xhr){
-			ProjectplaceViewController.user = new UserInfo(text);
-			
-			/**
-			 * Gets the data for all User projects.
-			 * Gets top 50 conversations for all projects ordered by last modified.
-			 * @param {Object} txt - json text
-			 * @param {Object} xhr - the xhr object
-			 */
-			PPAPI.getMyProjects(function(text, xhr){
+		* Gets the data for all User projects.
+		* Gets top 50 conversations for all projects ordered by last modified.
+		* @param {Object} txt - json text
+		* @param {Object} xhr - the xhr object
+		*/
+			PPAPI.getMyProjects(function (text, xhr) {
 				var projects = JSON.parse(text);
 				ProjectplaceViewController.user.saveProjects(projects);
 				for (var i = 0; i < projects.length; i++) {
-					PPAPI.projectConversations(projects[i].id,function(t, xhr){
+					PPAPI.projectConversations(projects[i].id, function (t, xhr) {
 							ProjectplaceViewController.user.setConversations(t);
 						}
 					);
 				}
 				/**
-				 * Get all coworkers for logged in user
-				 * @param {Object} text - JSONText
-				 * @param {Object} xhr - xhr object
-				 */
-				PPAPI.getMyCoWorkers(function(text,xhr){
+				* Get all coworkers for logged in user
+				* @param {Object} text - JSONText
+				* @param {Object} xhr - xhr object
+				*/
+				PPAPI.getMyCoWorkers(function (text, xhr) {
 					var coworkers = JSON.parse(text);
 					ProjectplaceViewController.user.saveCoworkers(coworkers);
-					
+						
 				});
 			});
 		});		
 	}
- }
+};
